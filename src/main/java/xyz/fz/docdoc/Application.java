7e6942ea66ca21fc12c64f9e9baa07e60ff6959b
fz <natureorz@foxmail.com>
2019-04-08 16:28:24 +0800

    upgrade

--- src/main/java/xyz/fz/docdoc/Application.java
+++ src/main/java/xyz/fz/docdoc/Application.java
@@ -2,6 +2,7 @@ package xyz.fz.docdoc;
 
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -34,21 +35,24 @@ public class Application {
         System.setProperty("vertx.disableFileCaching", Objects.requireNonNull(context.getEnvironment().getProperty("vertx.disableFileCaching")));
 
         /* deploy verticles */
+        // default VertxOptions's shared worker pool size is 20 (see VertxOptions)
         final Vertx vertx = Vertx.vertx();
 
-        // business verticles
-        int cpuCores = Runtime.getRuntime().availableProcessors();
+        int serverPort = Integer.parseInt(Objects.requireNonNull(context.getEnvironment().getProperty("server.port")));
         DeploymentOptions workerDeploymentOptions = new DeploymentOptions();
+        // use default shared worker pool if work pool name not set (see vertx.deploy src)
         workerDeploymentOptions.setWorker(true);
-        workerDeploymentOptions.setWorkerPoolSize(cpuCores * 4);
-        for (int i = 0; i < cpuCores * 2; i++) {
+        for (int i = 0; i < VertxOptions.DEFAULT_EVENT_LOOP_POOL_SIZE; i++) {
+            // one verticle instance bind one event loop thread
+
+            // business verticles
             // can't use workerDeploymentOptions.setInstances(cpuCores * 2) because of new Object not reflect Object
             vertx.deployVerticle(new ServiceVerticle(context), workerDeploymentOptions);
-        }
 
-        // http verticle
-        int serverPort = Integer.parseInt(Objects.requireNonNull(context.getEnvironment().getProperty("server.port")));
-        vertx.deployVerticle(new HttpVerticle(serverPort));
+            // http verticle
+            // see HttpServer.listen src --> sharedHttpServer (one acceptor and more worker)
+            vertx.deployVerticle(new HttpVerticle(serverPort));
+        }
 
         LOGGER.info("Deployment done. Server is running at port: {}", serverPort);
     }
